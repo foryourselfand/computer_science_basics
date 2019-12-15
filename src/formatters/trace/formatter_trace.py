a9f3718ab2e31268dfe2a168fd12a5bb016ad434
foryourselfand <foryourselfand@gmail.com>
2019-12-16 02:14:25 +0300

    save sleep

--- src/formatters/trace/formatter_trace.py
+++ src/formatters/trace/formatter_trace.py
@@ -6,7 +6,7 @@ from src.formatters.formatter import Formatter
 
 class FormatterTrace(Formatter, ABC):
     def __init__(self):
-        self._line_formatted = '{:>7s} {:>8s} {:>4s} {:>5s} {:>4s} {:>5s} {:>4s} {:>5s} {:>5s} {:>5s} {:>12s} {:>10s}'
+        self._line_formatted = '{:6s}\t{:7s}\t{:3s}\t{:4s}\t{:3s}\t{:4s}\t{:3s}\t{:4s}\t{:4s}\t{:3s}\t{:11s}\t{:9s}'
     
     def _get_header(self) -> List[str]:
         return ['Адресс', 'Код', 'IP', 'CR', 'AR', 'DR', 'SP', 'BR', 'AC', 'NZVC', 'Новый адрес', 'Новый Код']
